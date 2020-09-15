b695fb6c85af7e1e9001902fb5d6e2aa685a7dd3
Riz <joshua.riz.layton@gmail.com>
2020-09-15 02:57:19 +0100

    Fixed Gallery and GalleryCard components

--- src/components/UI/Gallery/Gallery.component.jsx
+++ src/components/UI/Gallery/Gallery.component.jsx
@@ -11,41 +11,41 @@ function Gallery() {
     <GalleryContainer>
       <GalleryItem>
         <GalleryCard
+          cardText="One Response"
+          cardTitle="Toy ambulance on grey wood"
           cardImage="https://images.unsplash.com/photo-1502740479091-635887520276?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80"
-          cardText="OneResponse"
-          cardTitle="Toy ambulance on gray wood"
         />
       </GalleryItem>
 
       <GalleryItem>
         <GalleryCard
-          cardImage="https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+          cardText="Acronym Search"
+          cardTitle="Eurofighter Typhoon taking off"
+          cardImage="https://images.unsplash.com/photo-1580028468862-6895981d7a67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+        />
+      </GalleryItem>
+
+      <GalleryItem>
+        <GalleryCard
           cardText="Rizji"
           cardTitle="Retro gaming technology"
+          cardImage="https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
         />
       </GalleryItem>
 
       <GalleryItem>
         <GalleryCard
-          cardImage="https://images.unsplash.com/photo-1580028468862-6895981d7a67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-          cardText="Acronym Search"
-          cardTitle="Eurofighter Typhoon jet taking off"
-        />
-      </GalleryItem>
-
-      <GalleryItem>
-        <GalleryCard
-          cardImage="https://images.unsplash.com/photo-1595225476474-87563907a212?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"
           cardText="Keyboard Shop"
-          cardTitle="Decorative mechanical keyboard and switch"
+          cardTitle="Decorative mechanical keyboard"
+          cardImage="https://images.unsplash.com/photo-1595225476474-87563907a212?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"
         />
       </GalleryItem>
 
       <GalleryItem>
         <GalleryCard
-          cardImage="https://images.unsplash.com/photo-1488998427799-e3362cec87c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-          cardText="Personal Website"
+          cardText="Portfolio Website"
           cardTitle="Phone, notebook, pencil, glasses and laptop"
+          cardImage="https://images.unsplash.com/photo-1488998427799-e3362cec87c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
         />
       </GalleryItem>
     </GalleryContainer>
@@ -59,9 +59,9 @@ export default Gallery;
 const GalleryContainer = styled.div`
   align-items: center;
   display: grid;
-  grid-gap: 1rem;
+  grid-gap: 1.6rem;
   grid-template-columns: repeat(2, 1fr);
-  margin: 1rem;
+  margin: 1.6rem;
   max-width: 100%;
 
   @media screen and (max-width: 800px) {
--- src/components/UI/GalleryCard/GalleryCard.component.jsx
+++ src/components/UI/GalleryCard/GalleryCard.component.jsx
@@ -2,77 +2,34 @@
 import React from "react";
 import styled from "styled-components";
 
-// UI: GalleryCard
-function GalleryCard({ cardImage, cardText, cardTitle }) {
+// UI: GCard
+function GCard({ cardImage, cardText, cardTitle }) {
   return (
-    <GalleryCardContainer>
-      <GalleryCardImage
-        style={{ backgroundImage: `url(${cardImage})` }}
-        title={cardTitle}
-      >
-        <GalleryTextButton>
-          <GalleryCardText>{cardText}</GalleryCardText>
-        </GalleryTextButton>
-      </GalleryCardImage>
-    </GalleryCardContainer>
+    <GCardContainer
+      style={{ backgroundImage: `url(${cardImage})` }}
+      title={cardTitle}
+    >
+      <p>{cardText}</p>
+    </GCardContainer>
   );
 }
 
-// Export: GalleryCard
-export default GalleryCard;
+// Export: GCard
+export default GCard;
 
-// Styled: GalleryCardContainer
-const GalleryCardContainer = styled.div`
-  height: 100%;
-  max-height: 100%;
-  object-fit: cover;
-  width: 100%;
-  max-width: 100%;
-`;
-
-// Styled: GalleryCardImage
-const GalleryCardImage = styled.div`
-  background-size: 100%;
-  background-repeat: no-repeat;
-  background-position: 50% 50%;
-  height: 100%;
-  max-height: 100%;
-  object-fit: cover;
-  width: 100%;
-  max-width: 100%;
-  transition: all 300ms ease-in-out;
-
-  &:hover {
-    background-size: 110%;
-    filter: grayscale(80%);
-    transition: all 300ms ease-in-out;
-  }
-`;
-
-// Styled: GalleryTextButton
-const GalleryTextButton = styled.div`
+// Styled: GCardContainer
+const GCardContainer = styled.div`
   background: ${(props) => props.theme.colors.global.backgroundPrimary};
-  cursor: pointer;
-  padding: 0.5rem 1rem;
-  transition: all 150ms linear;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  filter: brightness(0.9) saturate(95%);
+  height: 100%;
+  transition: all 300ms ease-in-out;
+  width: 100%;
 
   &:hover {
-    background: ${(props) => props.theme.colors.global.backgroundSecondary};
-    transition: all 150ms linear;
-  }
-`;
-
-// Styled: GalleryCardText
-const GalleryCardText = styled.h2`
-  color: ${(props) => props.theme.colors.global.textSecondary};
-  font-size: 1.4rem;
-  font-weight: 300;
-  letter-spacing: 2px;
-  text-transform: uppercase;
-  transition: all 150ms linear;
-
-  &:hover {
-    color: ${(props) => props.theme.colors.global.textPrimary};
-    transition: all 150ms linear;
+    filter: brightness(1) saturate(100%);
+    transition: all 300ms ease-in-out;
   }
 `;
