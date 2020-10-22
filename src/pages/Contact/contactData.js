0f9e3aaab612606a4f572c908bfe07f2e31714c0
Riz <joshua.riz.layton@gmail.com>
2020-10-22 18:35:09 +0100

    PF-4: Contact adjustments

--- src/pages/Contact/contactData.js
+++ src/pages/Contact/contactData.js
@@ -8,7 +8,7 @@ export const contactObjOne = {
   textOneFirst:
     "I'm currently looking for experiences and projects that can enhance my development skills, as well as my portfolio.",
   textOneSecond:
-    "If you'd like to get in touch, please use the contact form below to send a general message or details of a project you'd like me to be a part of and I'll get back to you as soon as possible.",
+    "If you'd like to get in touch, please use the contact form below to send a general message or details of a project you'd like me to be a part of, and I'll get back to you as soon as possible.",
   subHeadingTwo: null,
   subHeadingTwoLead: null,
   textTwoFirst: null,
